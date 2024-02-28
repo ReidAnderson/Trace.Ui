207f88badcf7085c6b6a5aadadaac42cc3fcfabb
Reid Anderson <rbarryanderson@gmail.com>
2024-02-27 20:20:36 -0600

    working with remove buttons

--- trace-ui/src/App.tsx
+++ trace-ui/src/App.tsx
@@ -2,10 +2,9 @@ import './App.css';
 import { styled } from '@mui/material/styles';
 import { Box, Paper, Button, Checkbox, Divider, FormControlLabel, Grid, Stack, TextField } from '@mui/material';
 import { TraceRequirement, Span } from './Interfaces';
-import { Formik, FormikErrors, Form, FieldArray, useFormikContext, Field } from 'formik';
 import { v4 as uuidv4 } from "uuid";
 import { FlameGraph, ServiceGraph, SpanTree } from './MermaidDefinitions';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import mermaid from "mermaid";
 
 type FormInputs = TraceRequirement & {
@@ -36,7 +35,7 @@ function getEmptySpan(traceId?: string, parentSpanId?: string): Span {
     status: '',
     attributes: {},
     links: [],
-    events: {}  
+    events: {}
   }
 }
 
@@ -54,103 +53,71 @@ export interface SpanProps {
   index: number;
 }
 
-function SpanDisplay(props: SpanProps) {
-  const { setFieldValue, values, ...formik } = useFormikContext<FormInputs>();
+function isValidJson(json: string): boolean {
+  try {
+    JSON.parse(json);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
 
+export interface TraceEditorProps {
+  inputSpans: Span[];
+  setSpans: (spans: Span[]) => void;
+}
+
+function TraceEditor(traceEditorProps: TraceEditorProps) {
+  const { inputSpans, setSpans } = traceEditorProps;
   return (
     <Grid container spacing={2}>
-
       <Grid item xs={12}>
-        <p>SpanId: {props.span.spanId}</p>
-        <p>ParentSpanId: {props.span.parentSpanId ?? "NONE"}</p>
-        <TextField
-          id="outlined-basic"
-          label="Span Name"
-          value={props.span.name}
-          onChange={(e) => setFieldValue(`${props.name}.${props.index}.spanName`, e.target.value)}
-          variant="outlined" />
-        <TextField
-          id="outlined-basic"
-          label="Service Name"
-          value={props.span.serviceName}
-          onChange={(e) => setFieldValue(`${props.name}.${props.index}.serviceName`, e.target.value)}
-          variant="outlined" />
+        <Item>Trace Editor</Item>
       </Grid>
       <Grid item xs={12}>
-        <TextField
-          id="outlined-basic"
-          label="Parent Service Name"
-          value={props.span.parentSpanId}
-          // TODO this is bad
-          variant="outlined" />
-        <Field
-          name={`${props.name}.${props.index}.useRegex`}
-          type="checkbox"
-          render={({ field }: any) => (<FormControlLabel
-            control={<Checkbox {...field} checked={field.value} />}
-            label="Use Regex"
-          />)} />
-
+        <Item>
+          <Button variant="contained" onClick={() => {
+            setSpans([...inputSpans, inputSpans[0]]);
+          }}>Add Span</Button>
+        </Item>
       </Grid>
-      {/* <Grid item xs={12}>
-        <TextField
-          id="duration-min"
-          label="Minimum Duration (ms)"
-          type="number"
-          value={props.span.minDuration}
-          onChange={(e) => setFieldValue(`${props.name}.${props.index}.minDuration`, e.target.value)}
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField
-          id="duration-max"
-          label="Maximum Duration (ms)"
-          type="number"
-          value={props.span.maxDuration}
-          onChange={(e) => setFieldValue(`${props.name}.${props.index}.maxDuration`, e.target.value)}
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-      </Grid> */}
-    </Grid>
-  );
-}
-
-export interface SpanListProps {
-  criteria: Span[];
-  name: string;
-}
-
-function SpanList(props: SpanListProps) {
-  return (
-    <FieldArray name={props.name}>
-      {(arrayHelpers) => (
-        <Stack
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-          divider={<Divider orientation="horizontal" flexItem />}
-          spacing={2}>
-          {props.criteria && props.criteria.length > 0 ? props.criteria.map((criteria, index) => {
+      <Grid item xs={12}>
+        <Item>
+          {inputSpans.map((span, index) => {
             return (
-              <>
-                <SpanDisplay span={criteria} name={props.name} index={index} />
-                <Button variant='contained' onClick={() => arrayHelpers.push(getEmptySpan(criteria.traceId, criteria.spanId))}>Add Child Span</Button>
-                <Button variant='contained' onClick={() => arrayHelpers.remove(index)}>Delete</Button>
-              </>
+              <Stack key={index} direction="row" spacing={2}>
+                <TextField
+                  id="outlined-multiline-static"
+                  label="Multiline"
+                  multiline
+                  rows={4}
+                  value={JSON.stringify(span, null, 2)}
+                  onChange={(e) => {
+                    let newSpans = [...inputSpans];
+                    newSpans[index] = JSON.parse(e.target.value);
+                    setSpans(newSpans);
+                  }}
+                  error={!isValidJson(JSON.stringify(span))}
+                  helperText={!isValidJson(JSON.stringify(span)) ? "Invalid JSON" : ""}
+                />
+                <Button variant="contained" onClick={() => {
+                  let newSpans = [...inputSpans];
+                  newSpans.splice(index, 1);
+                  setSpans(newSpans);
+                }}>Remove</Button>
+              </Stack>
             )
-          }) : (
-            <Button variant='contained' onClick={() => arrayHelpers.push(getEmptySpan())}>Add Span</Button>
-          )}
-        </Stack>)}
-    </FieldArray>
-  );
+          })}
+        </Item>
+      </Grid>
+    </Grid>
+  )
 }
 
 function FormContent() {
-  const { setFieldValue, values, errors, ...formik } = useFormikContext<FormInputs>();
+  const [spans, setSpans] = useState<Span[]>([]);
+  const [requiredSpanText, setRequiredSpanText] = useState('');
+  const [disallowedSpanText, setDisallowedSpanText] = useState('');
 
   useEffect(() => {
     // remove the data-processed html attribute from all elements with an id of mermaid
@@ -165,50 +132,21 @@ function FormContent() {
 
     mermaid.initialize({ startOnLoad: true });
     mermaid.run();
-  }, [values.requiredSpans])
+  }, [spans])
 
   useEffect(() => {
-    
-  }, [values.requiredSpansText, values.disallowedSpansText])
-
-  function spanSet(field:string, fieldValue: string) {
-
     let parsedJson = [] as Span[];
 
     try {
-      parsedJson = JSON.parse(fieldValue);
-      setFieldValue(field, parsedJson);
+      parsedJson = JSON.parse(requiredSpanText);
+      setSpans(parsedJson);
     } catch (e) {
-      errors.requiredSpans = "Invalid JSON";
+      console.error("Invalid JSON");
     }
-  }
+  }, [requiredSpanText])
 
   return (
     <Grid container spacing={2}>
-      <Grid item xs={4} hidden>
-        <Item>Span Filter: Any trace containing a span that matches the following criteria will be evaluated
-          <Stack
-            direction="column"
-            justifyContent="center"
-            alignItems="flex-start"
-            spacing={2}
-          >
-            <TextField id="outlined-basic" label="Span Name" variant="outlined" />
-            <TextField id="outlined-basic" label="Service Name" variant="outlined" />
-            <FormControlLabel control={<Checkbox defaultChecked />} label="Use Regex" />
-          </Stack>
-        </Item>
-      </Grid>
-      {/* <Grid item xs={4} hidden>
-        <Item>Required Spans: The trace must contain a match for all of the following spans
-          <SpanList criteria={values.requiredSpans} name="requiredSpans" />
-        </Item>
-      </Grid>
-      <Grid item xs={4} hidden>
-        <Item>Disallowed Spans: The trace cannot contain a match for any of the following spans
-          <SpanList criteria={values.disallowedSpans} name="disallowedSpans" />
-        </Item>
-      </Grid> */}
       <Grid item xs={6}>
         <Item>Required Spans Trace:
           <TextField
@@ -216,8 +154,10 @@ function FormContent() {
             label="Multiline"
             multiline
             rows={4}
-            value={JSON.stringify(values.requiredSpans)}
-            onChange={(e) => spanSet('requiredSpans', e.target.value)}
+            value={requiredSpanText}
+            onChange={(e) => setRequiredSpanText(e.target.value)}
+            error={!isValidJson(requiredSpanText)}
+            helperText={!isValidJson(requiredSpanText) ? "Invalid JSON" : ""}
           />
         </Item>
       </Grid>
@@ -228,18 +168,23 @@ function FormContent() {
             label="Multiline"
             multiline
             rows={4}
-            value={JSON.stringify(values.disallowedSpans)}
-            onChange={(e) => setFieldValue('disallowedSpans', JSON.parse(e.target.value))}
+            value={disallowedSpanText}
+            onChange={(e) => setDisallowedSpanText(e.target.value)}
+            error={!isValidJson(disallowedSpanText)}
+            helperText={!isValidJson(disallowedSpanText) ? "Invalid JSON" : ""}
           />
         </Item>
       </Grid>
       <Grid item xs={12}>
+        <TraceEditor inputSpans={spans} setSpans={setSpans} />
+      </Grid>
+      <Grid item xs={12}>
         <Item>Required Spans Visualization
-          <FlameGraph trace={values.requiredSpans} />
+          <FlameGraph trace={spans} />
           <hr />
-          <SpanTree trace={values.requiredSpans} />
+          <SpanTree trace={spans} />
           <hr />
-          <ServiceGraph trace={values.requiredSpans} />
+          <ServiceGraph trace={spans} />
         </Item>
       </Grid>
       <Grid item xs={12}>
@@ -265,29 +210,9 @@ function FormContent() {
 function App() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Formik<FormInputs>
-        initialValues={initialValues}
-        onSubmit={submit}
-        validate={validate}
-      >
-        <Form>
-          <FormContent />
-        </Form>
-      </Formik>
+      <FormContent />
     </Box>
   );
-
-  function validate(values: FormInputs) {
-    const errors: FormikErrors<FormInputs> = {};
-
-    return {
-      ...errors
-    };
-  }
-
-  async function submit(values: FormInputs) {
-    return;
-  }
 }
 
 export default App;
